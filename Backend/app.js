b47ee691539f5bb0468c1a525ae3942c5e842d59
Parvesh M <parvesh@gmail.com>
2024-01-21 00:08:17 +0530

    Cors enabled with options

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 8000;
 const router = require("./routers/routes");
 const connectDB = require("./config/dbconfig");
 
+var corsOptions = {
+  origin: `http://localhost:${port}`,
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 // user Routes
